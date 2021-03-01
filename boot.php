0802aec2b84b24d00ddc968e20fde4c768eba6c0
Thomas Skerbis <thomas.skerbis@mac.com>
2021-03-01 11:47:33 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,11 +1,11 @@
 <?php
-
+$package = rex_addon::get('locked');
 // Add status for locked articles and categories
 
 if (rex::isBackend()) {
     rex_extension::register(['ART_STATUS_TYPES', 'CAT_STATUS_TYPES'], function (rex_extension_point $ep) {
         $subject = $ep->getSubject();
-        $subject[] = ['gesperrt', 'rex-offline', 'fa fa-exclamation-triangle'];
+        $subject[] = [$package->i18n('quicknavi_ignoreoffline'), 'rex-offline', 'fa fa-exclamation-triangle'];
         $ep->setSubject($subject);
         return $ep->getSubject();
     });
